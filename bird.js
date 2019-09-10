0bfc19b4ab7e3f79af5e9fa421f53768e5711c0e
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2019-09-10 23:40:10 +0100

    correct hitbox sizes

--- bird.js
+++ bird.js
@@ -8,7 +8,7 @@ function Bird() {
 
   this.show = function() {
     fill(255, 50);
-    rect(this.x, this.y, 100, 100);
+    rect(this.x, this.y, this.r, this.r);
     image(avatar, this.x, this.y, this.r, this.r);
   }
 
