587e66f8884f9cd8698152f97582efdc4a77fc8f
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2019-09-25 23:49:48 +0100

    implement touch screen functionality

--- sketch.js
+++ sketch.js
@@ -51,20 +51,25 @@ function draw() {
         text('GAME OVER', width / 2, height / 2);
         textAlign(LEFT, BASELINE);
         noLoop();
-        pipes = [];
-        game_speed = 6
-        bird = new Bird();
-        pipes.push(new Pipe(game_speed));
-        score = new Score();
+
       }
 
       if(pipes[i].offscreen()) {
         pipes.splice(i, 1);
       }
-    }
+
+      if (touches.length > 0) {
+          bird.jump;
+      }
+  }
 }
 
 function keyPressed() {
   if(key == ' ')
     bird.jump();
+}
+
+function touchStarted([event]) {
+    bird.jump;
+    return false;
 }
